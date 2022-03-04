66f9061a210e08837d8be9e57a8d73b0aa0a0605
FREAK <100413283+FREAKNOLIMIT@users.noreply.github.com>
2022-03-04 16:52:33 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Nezuko_Chan" //namabot kalian
+global.ownername= "@Abidin" //nama kalian
+global.myweb ="https://github.com/FREAKNOLIMIT" //bebas asal jan hapus
+global.youtube = "https://www.youtube.com/channel/UCQ5dDSqMCTISiov3Z7juQ8w" //bebas asal jan hapus
+global.github = "https://github.com/DintzOnly" //bebas
+global.email = "endangsoekamti021@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "6281218045371" // nomor wa kalian
+global.ownernomerr = "+6281218045371" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["6281218045371","6281218045371","6281218045371"] //ganti agar fitur owner bisa di gunakan
+global.packname = 'Dintz' //sticker wm ubah
+global.author = 'Di Buat Oleh DintzOnly' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
